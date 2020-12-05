1fd748522a03cd65dabf9dd375ce0af35aaaf884
taisey <taisey02@gmail.com>
2020-12-05 17:35:51 +0900

    re-name

--- poker_class.py
+++ poker_class.py
@@ -4,13 +4,13 @@ class PlayCard():
     def __init__(self):
         self.deck=[]
     def deckset(self):
-        self.deck=[a+str(b) for a in ["♤ ","♧ ","♡ ","♢ "] for b in range(1,14)]
+        self.deck=[a+str(b) for a in ["♤","♧","♡","♢"] for b in range(1,14)]
     def deckset_j1(self):
-        self.deck=[a+str(b) for a in ["♤ ","♧ ","♡ ","♢ "] for b in range(1,14)]
+        self.deck=[a+str(b) for a in ["♤","♧","♡","♢"] for b in range(1,14)]
         self.deck.append("joker")
 
     def deckset_j2(self):
-        self.deck=[a+str(b) for a in ["♤ ","♧ ","♡ ","♢ "] for b in range(1,14)]
+        self.deck=[a+str(b) for a in ["♤","♧","♡","♢"] for b in range(1,14)]
         self.deck.append("joker")
         self.deck.append("joker")
 
@@ -58,9 +58,9 @@ class PlayCard():
             ans.append(self.deck.pop(0))
         return ans
     def drow(self,num,card):
-
+        
         a=self.deal(num)
-        card=card + a
+        card=card+a
     def discard(self,num,card):
         cntr=0
         for i in sorted(num):
@@ -175,107 +175,25 @@ class Poker(PlayCard):
         score=self.onepair_is*1+self.twopair_is*2+self.threecard_is*3+self.flush_is*4
         +self.fullhouse_is*5+self.fourcard_is*6+self.straight_is*7+self.royal_straight_is*8
         +self.royal_straight_flush_is*9
+        
         return score
     
-
         
     def gamestart(self):
         player1=play.deal(5)
         print(player1)
         print("score: ",self.getscore(player1))
         
-class Player():
-    def __init__(self):
-        self.hand=[]
-        self.money=0
-        self.human=False
-    def gethand(self,hand):
-        self.hand=hand
-    def moneychange(self,money):
-        self.money=self.money+money
-    def showhand(self):
-        tmp_hand=self.hand
-        return tmp_hand
-    def is_human(self):
-        self.human=True
-    def showmoney(self):
-        return self.money
-    def drow(self,hand):
-        self.hand=self.hand+hand
-
-class Dealer():
-    def __init__(self):
-        self.hand=[]
-        self.money=0
-    def gethand(self,hand):
-        self.hand=hand
-    def moneychange(self,money):
-        self.money=self.money+money
-    def showhand(self,num):
-        tmp_hand=[]
-        for i in range(0,num):
-            tmp_hand.append(self.hand[i])
-        return tmp_hand
-    def drow(self,hand):
-        self.hand=self.hand+hand
-
-
-play=Poker()
-N=4
-nowplayer=N
-money=1000
-dealmoney=0
-playerNum=1
-fieldmoney=0
-dealerhandcntr=3
-playerlist=[ Player() for i in range(0,N)]
-playeralive=[ 1 for i in range(0,N)]
-for i in range(0,playerNum):
-    playerlist[i].is_human()
-dealer=Dealer()
-
-for i in playerlist:
-    i.gethand(play.deal(2))
-    i.moneychange(money)
-dealer.gethand(play.deal(5))
-
-while(dealerhandcntr<=5 and True):
+        
+#play=Poker()
+#play.gamestart()  
+        
+        
+        
+        
     
-    for i in range(0,N):
-        if(playeralive[i]!=1):
-            continue
-        print("player%d" %(i+1))
-        print("dealer: ",dealer.showhand(dealerhandcntr))
-        print("yourhand: ",playerlist[i].showhand())
-        print("bet: ",fieldmoney)
-
-        s=input("choose call,raise,fold\n\n")
-        #inputの例外処理をのちに追加する
-        if (s=="fold"):
-            playerlist[i].moneychange(fieldmoney*-1)
-            print("accept")
-            print("--------------\n")
-            playeralive[i]=0
-            continue
-        elif(s=="call"):
-            print("accept") 
-            print("--------------\n")
-            continue
-        elif(s=="raise"):
-            print("bet: ",fieldmoney)
-            print("your money: ",playerlist[i].showmoney())
-            s_raise=int(input("how much?"))
-            if (s_raise>fieldmoney and s_raise<=playerlist[i].showmoney()):
-                print("accept")
-                print("--------------\n")
-                fieldmoney=s_raise
-            continue
- 
-    dealerhandcntr+=1
-
-    
-
-
-
+                
+        
+        
 
 
